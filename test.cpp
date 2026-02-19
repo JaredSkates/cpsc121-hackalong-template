22bf5fe03b29b41078461fd22dd41b99437343b6
Jared Anastacio Ferrer <152446052+JaredSkates@users.noreply.github.com>
2026-02-19 19:53:37 +0000

    Implemented Template Class

--- test.cpp
+++ test.cpp
@@ -1,1 +1,41 @@
+// Jared Anastacio Ferrer
+// 835118894
 
+#include <string>
+#include <iostream>
+
+template <typename T, typename U>
+class box {
+    public:
+    box(T item, U other_item) : item_(item), other_item_(other_item) {}
+
+    T getItem() const {
+        return item_;
+    }
+
+    void setItem(T item, U other_item) {
+        item_ = item;
+        other_item_ = other_item;
+    }
+
+    void Print() {
+        std::cout << "The first item is " 
+                  << item_ 
+                  << " The second item is " 
+                  << other_item_ 
+                  << std::endl;
+    }
+
+    private:
+    T item_;
+    U other_item_;
+};
+
+
+int main() {
+    box b_pair(5, 2.2);
+
+    b_pair.Print();
+   
+    return 0;
+}
